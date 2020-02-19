21783711a61beb678923a72a21e675c8a30950b2
thiery <thiery@marmelab.com>
2020-02-19 17:10:51 +0100

    parse docker engine api response

--- cli/index.js
+++ cli/index.js
@@ -3,6 +3,25 @@ const readline = require("readline");
 
 const containerName = "argos_cypress_1";
 
+const get = path => data =>
+    path.reduce((subData, key) => (subData ? subData[key] : undefined), data);
+
+const getTotalCpu = get(["cpu_stats", "system_cpu_usage"]);
+const getCpuUsage = get(["cpu_stats", "cpu_usage", "total_usage"]);
+const getKernelCpuUsage = get([
+    "cpu_stats",
+    "cpu_usage",
+    "usage_in_kernelmode",
+]);
+const getUserCpuUsage = get(["cpu_stats", "cpu_usage", "usage_in_usermode"]);
+
+const getMemoryUsage = get(["memory_stats", "usage"]);
+const getMemoryMaxUsage = get(["memory_stats", "max_usage"]);
+const getMemoryLimit = get(["memory_stats", "limit"]);
+
+const getReceivedNetwork = get(["networks", "eth0", "rx_bytes"]);
+const getTransmittedNetwork = get(["networks", "eth0", "tx_bytes"]);
+
 const initGetValueIncrement = () => {
     let previousValue = 0;
     return value => {
@@ -12,6 +31,9 @@ const initGetValueIncrement = () => {
         return result;
     };
 };
+
+const getCurrentReceivedNetwork = initGetValueIncrement();
+const getCurrentTransmittedNetwork = initGetValueIncrement();
 
 const run = async () => {
     input = spawn("curl", [
@@ -29,7 +51,43 @@ const run = async () => {
 
     return new Promise((resolve, reject) => {
         readInterface.on("line", function(line) {
-            console.log(JSON.parse(line));
+            const json = JSON.parse(line);
+
+            const totalCpu = getTotalCpu(json);
+            const cpuUsage = getCpuUsage(json);
+            const kernelCpuUsage = getKernelCpuUsage(json);
+            const userCpuUsage = getUserCpuUsage(json);
+
+            const totalReceivedNetwork = getReceivedNetwork(json);
+            const currentReceivedNetwork = getCurrentReceivedNetwork(
+                totalReceivedNetwork
+            );
+
+            const totalTransmittedNetwork = getTransmittedNetwork(json);
+            const currentTransmittedNetwork = getCurrentTransmittedNetwork(
+                totalReceivedNetwork
+            );
+
+            const result = {
+                cpu: {
+                    totalCpu,
+                    cpuUsage,
+                    kernelCpuUsage,
+                    userCpuUsage,
+                },
+                memory: {
+                    usage: getMemoryUsage(json),
+                    maxUsage: getMemoryMaxUsage(json),
+                    limit: getMemoryLimit(json),
+                },
+                network: {
+                    totalReceived: totalReceivedNetwork,
+                    currentReceived: currentReceivedNetwork,
+                    totalTransmitted: totalTransmittedNetwork,
+                    currentTransmitted: currentTransmittedNetwork,
+                },
+            };
+            console.log(result);
         });
     });
 };
