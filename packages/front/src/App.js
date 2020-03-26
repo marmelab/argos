35ea1e603e00d674ea6e4b2f7360c48eaac0ba47
thiery <thiery@marmelab.com>
2020-03-26 11:48:22 +0100

    rename App to Stats add new App

--- packages/front/src/App.js
+++ packages/front/src/App.js
@@ -1,41 +1,9 @@
 import React from 'react';
-import { Chart } from './Chart';
 
-import { useFetch } from './useFetch';
+import { Stats } from './Stats';
+
+const containerName = 'angry_mayer';
 
 export const App = () => {
-    const { isLoading, response } = useFetch('http://localhost:3002/angry_mayer');
-
-    if (isLoading || !response) {
-        return '...';
-    }
-
-    const cpuPercentage = response.map(({ date, cpu }) => ({
-        date: new Date(date),
-        value: cpu.cpuPercentage || 0,
-    }));
-
-    const memoryUsage = response.map(({ date, memory }) => ({
-        date: new Date(date),
-        value: memory.usage || 0,
-    }));
-
-    const networkReceived = response.map(({ date, network }) => ({
-        date: new Date(date),
-        value: network.currentReceived || 0,
-    }));
-
-    const networkEmitted = response.map(({ date, network }) => ({
-        date: new Date(date),
-        value: network.currentEmitted || 0,
-    }));
-
-    return (
-        <div>
-            <Chart title="cpu percentage" data={cpuPercentage} />
-            <Chart title="memory usage" data={memoryUsage} />
-            <Chart title="network received" data={networkReceived} />
-            <Chart title="network emitted" data={networkEmitted} />
-        </div>
-    );
+    return <Stats container={containerName} />;
 };
---
+++ packages/front/src/Stats.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { Chart } from './Chart';
+
+import { useFetch } from './useFetch';
+
+export const Stats = ({ container }) => {
+    const { isLoading, response } = useFetch(`http://localhost:3002/${container}`);
+
+    if (isLoading || !response) {
+        return '...';
+    }
+
+    const cpuPercentage = response.map(({ date, cpu }) => ({
+        date: new Date(date),
+        value: cpu.cpuPercentage || 0,
+    }));
+
+    const memoryUsage = response.map(({ date, memory }) => ({
+        date: new Date(date),
+        value: memory.usage || 0,
+    }));
+
+    const networkReceived = response.map(({ date, network }) => ({
+        date: new Date(date),
+        value: network.currentReceived || 0,
+    }));
+
+    const networkEmitted = response.map(({ date, network }) => ({
+        date: new Date(date),
+        value: network.currentEmitted || 0,
+    }));
+
+    return (
+        <div>
+            <Chart title="cpu percentage" data={cpuPercentage} />
+            <Chart title="memory usage" data={memoryUsage} />
+            <Chart title="network received" data={networkReceived} />
+            <Chart title="network emitted" data={networkEmitted} />
+        </div>
+    );
+};
