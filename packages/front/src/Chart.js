b87dff8632332a7999f490ae43b242d6a018cffe
thiery <thiery@marmelab.com>
2020-04-15 18:30:51 +0200

    display average and median values

--- packages/front/src/Chart.js
+++ packages/front/src/Chart.js
@@ -3,10 +3,25 @@ import { LineChart, XAxis, YAxis, Line, Legend } from 'recharts';
 
 const colors = ['#006699', '#009933', '#cc9900', '#cc0000', '#cc00cc', '#3333ff'];
 
+const getAverage = values => values.reduce((acc, v) => acc + v, 0) / values.length;
+const getMedian = values => values.sort((a, b) => a - b)[Math.round(values.length / 2)];
 export const Chart = ({ title, data, lineKeys }) => {
     return (
         <div>
             <h2>{title}</h2>
+            {lineKeys.map(measure => {
+                const values = data.map(v => v[measure] || 0);
+                return (
+                    <div>
+                        <h3>{measure}</h3>
+                        <ul>
+                            <li>average:{getAverage(values).toFixed(2)}</li>
+                            <li>median:{getMedian(values).toFixed(2)}</li>
+                        </ul>
+                    </div>
+                );
+            })}
+            <p>average: </p>
             <LineChart width={1000} height={300} data={data}>
                 <Legend verticalAlign="top" height={36} />
                 <XAxis dataKey="time" />
