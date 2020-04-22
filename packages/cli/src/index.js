d430271e156af077e84852eac58076efb1cdb2f9
thiery <thiery@marmelab.com>
2020-04-22 14:08:18 +0200

    have cli launch the command we which to analyze

--- packages/cli/src/index.js
+++ packages/cli/src/index.js
@@ -1,10 +1,21 @@
-const fs = require('fs');
 const getContainerStats = require('./getContainerStats');
 const onStartContainer = require('./onStartContainer');
+const { promisify } = require('util');
+const exec = promisify(require('child_process').exec);
 
 const run = async () => {
     const measureName = process.env.NAME;
+    const command = process.env.COMMAND;
+
     onStartContainer(getContainerStats(measureName));
+
+    const { stdout, stderr } = await exec(command);
+
+    if (stderr) {
+        console.error(stderr);
+    }
+
+    console.log(`child process exited`);
 };
 
 run();
