00caefde99367d974deb29e1ca6232da881883d0
thiery <thiery@marmelab.com>
2020-05-07 16:29:19 +0200

    allow to pass list of containers to listen too

--- packages/cli/src/index.js
+++ packages/cli/src/index.js
@@ -7,12 +7,16 @@ const run = async () => {
     const measureName = process.env.NAME;
     const command = process.env.COMMAND;
     const runQuantity = process.env.RUN_QUANTITY;
+    const otherContainers = process.env.OTHER_CONTAINERS ? process.env.OTHER_CONTAINERS.split(',') : [];
 
     for (var i = 1; i <= runQuantity; i++) {
         console.info(`run ${i}:`);
+
+        const measuresStoppers = otherContainers.map(getContainerStats(measureName, i));
         const stopListening = onStartContainer(getContainerStats(measureName, i));
         await exec(command).catch(console.warn);
         stopListening();
+        measuresStoppers.map(stop => stop());
     }
 
     await computeMeasureAverage(measureName);
