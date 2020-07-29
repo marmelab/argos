0abeb90e8182a0397a39009e5d2d30d7bc2c9e42
thiery <thiery@marmelab.com>
2020-07-29 11:40:09 +0200

    AFEM-888: add SETUP_COMMAND arg to be able to run test setup only once before every run

--- packages/cli/src/index.js
+++ packages/cli/src/index.js
@@ -6,6 +6,7 @@ const getLastRun = require('./getLastRun');
 
 const run = async () => {
     const measureName = process.env.NAME;
+    const setupCommand = process.env.SETUP_COMMAND;
     const command = process.env.COMMAND;
     const runQuantity = process.env.RUN_QUANTITY;
     const otherContainers = process.env.OTHER_CONTAINERS ? process.env.OTHER_CONTAINERS.split(',') : [];
@@ -16,7 +17,11 @@ const run = async () => {
         console.log(`${lastRun} previous runs detected for measure ${measureName}, adding run to the existing ones.`);
     }
 
-    for (var i = lastRun + 1; i <= lastRun + parseInt(runQuantity, 10); i++) {
+    if (setupCommand) {
+        await exec(setupCommand).catch(console.warn);
+    }
+
+    for (var i = lastRun + 1; i <= lastRun + runQuantity; i++) {
         console.info(`run ${i}:`);
 
         const measuresStoppers = otherContainers.map(getContainerStats(measureName, i));
